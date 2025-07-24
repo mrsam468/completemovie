265e492cbb12ad157d261d7ec6ad6bb220411c78
samuel <ibenjaminsamuel47@gmail.com>
2025-07-24 20:37:56 +0100

    trending movies set up

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-
+const global={
+  page:window.location.pathname,
+}
+console.log(global.page)
 async function fetchapidata(api_url) {
   const apikey = "3fd2be6f0c70a2a598f084ddfb75487c";
   const BaseUrl = "https://api.themoviedb.org/3/";
@@ -28,4 +31,50 @@ div.innerHTML=`
 document.querySelector("#title-div").appendChild(div);
 
 }   
+async function trends(){
+const url='https://api.themoviedb.org/3/trending/all/day?language=en-US' ;
+const result =await fetch(url,{
+  method:"GET", 
+  headers:{
+    Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YzBkZjBlZDNiMjIyMDgwMDc3NzM2Mzg4ODEwMGM4ZiIsIm5iZiI6MTc0OTc5NTczOS42NjIwMDAyLCJzdWIiOiI2ODRiYzM5YjFmNmQxOGU2NTEzZmRlZGQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.9KOzF92SpnaBDZiJGNFUiJlX52JyFSTJNv1FHiJF-Yw",
+    Accept:"application/json"
+  }
+})
+const data=await result.json()
+const results=data.results;
+results.forEach(card => {
+  const div=document.createElement("div");
+  div.classList.add("card")
+  div.innerHTML=`<img id="card-image" src=https://image.tmdb.org/t/p/w500/${card.poster_path} alt="${card.title}">`
+  document.querySelector("#genre-two").appendChild(div)
+});
+}
+async function seemore(){
+const url='https://api.themoviedb.org/3/trending/all/day?language=en-US' ;
+const result =await fetch(url,{
+  method:"GET", 
+  headers:{
+    Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YzBkZjBlZDNiMjIyMDgwMDc3NzM2Mzg4ODEwMGM4ZiIsIm5iZiI6MTc0OTc5NTczOS42NjIwMDAyLCJzdWIiOiI2ODRiYzM5YjFmNmQxOGU2NTEzZmRlZGQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.9KOzF92SpnaBDZiJGNFUiJlX52JyFSTJNv1FHiJF-Yw",
+    Accept:"application/json"
+  }
+})
+const data=await result.json()
+const results=data.results;
+results.forEach(card => {
+  const div=document.createElement("div");
+  div.classList.add("card")
+  div.innerHTML=`<img id="card-image" src=https://image.tmdb.org/t/p/w500/${card.poster_path} alt="${card.title}">`
+  document.querySelector("#see-more-section").appendChild(div)
+});
+}
+switch(global.page){
+  case "/index.html":
+    trends()
  backgroundImage()
+ break;
+
+ case "/trend.html":
+ seemore()
+ break;
+
+}
