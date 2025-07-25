3d7a069468c854643d82fcca70843266ae938bbd
samuel <ibenjaminsamuel47@gmail.com>
2025-07-25 22:07:23 +0100

    git adjustment

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const global={
-  page:window.location.pathname,
-}
-const baseurl="https://api.themoviedb.org/3/";
-const baseurl_image="https://image.tmdb.org/t/p/w500/"
-const trend ="/trending/all/day?language=en-US";
+const global = {
+  page: window.location.pathname,
+};
+const baseurl = "https://api.themoviedb.org/3/";
+const baseurl_image = "https://image.tmdb.org/t/p/w500/";
+const trend = "/trending/all/day?language=en-US";
 const movie = "/movie/popular?language=en-US&page=1";
 const serie = "/tv/popular?language=en-US&page=1";
-
 
 async function fetchapidata(api_url) {
   const apikey = "3fd2be6f0c70a2a598f084ddfb75487c";
@@ -16,7 +15,6 @@ async function fetchapidata(api_url) {
   );
   return res.json();
 }
-
 
 async function backgroundImage() {
   const { results } = await fetchapidata("movie/popular");
@@ -28,211 +26,220 @@ async function backgroundImage() {
   welcome.style.backgroundImage = `url(${baseurl_image}${randomIndex.backdrop_path})`;
   welcome.style.backgroundSize = "cover";
   welcome.style.backgroundPosition = "center";
-const div = document.createElement("div");
-div.classList.add("main-title")
-div.innerHTML=`
+  const div = document.createElement("div");
+  div.classList.add("main-title");
+  div.innerHTML = `
 <h2 id="witcher-text">${randomIndex.title}</h2>
 <P id="info-text">${randomIndex.overview}</P>
-`
-document.querySelector("#title-div").appendChild(div);
-
-}   
-async function trends(){
-const url=baseurl+  trend ;
-const result =await fetch(url,{
-  method:"GET", 
-  headers:{
-    Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YzBkZjBlZDNiMjIyMDgwMDc3NzM2Mzg4ODEwMGM4ZiIsIm5iZiI6MTc0OTc5NTczOS42NjIwMDAyLCJzdWIiOiI2ODRiYzM5YjFmNmQxOGU2NTEzZmRlZGQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.9KOzF92SpnaBDZiJGNFUiJlX52JyFSTJNv1FHiJF-Yw",
-    Accept:"application/json"
+`;
+  document.querySelector("#title-div").appendChild(div);
+}
+async function trends() {
+  try {
+    const url = baseurl + trend;
+    const result = await fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YzBkZjBlZDNiMjIyMDgwMDc3NzM2Mzg4ODEwMGM4ZiIsIm5iZiI6MTc0OTc5NTczOS42NjIwMDAyLCJzdWIiOiI2ODRiYzM5YjFmNmQxOGU2NTEzZmRlZGQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.9KOzF92SpnaBDZiJGNFUiJlX52JyFSTJNv1FHiJF-Yw",
+        Accept: "application/json",
+      },
+    });
+    const data = await result.json();
+    const results = data.results;
+    results.forEach((card) => {
+      const div = document.createElement("div");
+      div.classList.add("card");
+      div.innerHTML = `<img id="card-image" src="${baseurl_image}${
+        card.poster_path
+      }" alt="${card.title || card.name}"/>`;
+      document.querySelector("#genre-two").appendChild(div);
+    });
+  } catch (error) {
+    console.log(error);
   }
-})
-const data=await result.json()
-const results=data.results;
-results.forEach(card => {
-  const div=document.createElement("div");
-  div.classList.add("card")
-  div.innerHTML=`<img id="card-image" src=${baseurl_image}${card.poster_path} alt="${card.title}">`
-  document.querySelector("#genre-two").appendChild(div)
-});
 }
-async function seemore(){
-const url=baseurl+trend ;
-const result =await fetch(url,{
-  method:"GET", 
-  headers:{
-    Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YzBkZjBlZDNiMjIyMDgwMDc3NzM2Mzg4ODEwMGM4ZiIsIm5iZiI6MTc0OTc5NTczOS42NjIwMDAyLCJzdWIiOiI2ODRiYzM5YjFmNmQxOGU2NTEzZmRlZGQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.9KOzF92SpnaBDZiJGNFUiJlX52JyFSTJNv1FHiJF-Yw",
-    Accept:"application/json"
-  }
-})
-const data=await result.json()
-const results=data.results;
-results.forEach(card => {
-  const div=document.createElement("div");
-  div.classList.add("card")
-  div.innerHTML=`<img id="card-image" src=${baseurl_image}${card.poster_path} alt="${card.title}">`
-  document.querySelector("#see-more-section").appendChild(div)
-});
+async function seemore() {
+  const url = `${baseurl}${trend}`;
+  const result = await fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YzBkZjBlZDNiMjIyMDgwMDc3NzM2Mzg4ODEwMGM4ZiIsIm5iZiI6MTc0OTc5NTczOS42NjIwMDAyLCJzdWIiOiI2ODRiYzM5YjFmNmQxOGU2NTEzZmRlZGQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.9KOzF92SpnaBDZiJGNFUiJlX52JyFSTJNv1FHiJF-Yw",
+      Accept: "application/json",
+    },
+  });
+  const data = await result.json();
+  const results = data.results;
+  results.forEach((card) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `<img id="card-image" src=${baseurl_image}${card.poster_path} alt="${card.title}">`;
+    document.querySelector("#see-more-section").appendChild(div);
+  });
 }
 
-async function movies(){
-const url=baseurl+  movie ;
-const result =await fetch(url,{
-  method:"GET", 
-  headers:{
-    Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YzBkZjBlZDNiMjIyMDgwMDc3NzM2Mzg4ODEwMGM4ZiIsIm5iZiI6MTc0OTc5NTczOS42NjIwMDAyLCJzdWIiOiI2ODRiYzM5YjFmNmQxOGU2NTEzZmRlZGQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.9KOzF92SpnaBDZiJGNFUiJlX52JyFSTJNv1FHiJF-Yw",
-    Accept:"application/json"
-  }
-})
-const data=await result.json()
-const results=data.results;
-results.forEach(card => {
-  const div=document.createElement("div");
-  div.classList.add("card")
-  div.innerHTML=`<img id="card-image" src=${baseurl_image}${card.poster_path} alt="${card.title}">`
-  document.querySelector("#genre-three").appendChild(div)
-});
+async function movies() {
+  const url = baseurl + movie;
+  const result = await fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YzBkZjBlZDNiMjIyMDgwMDc3NzM2Mzg4ODEwMGM4ZiIsIm5iZiI6MTc0OTc5NTczOS42NjIwMDAyLCJzdWIiOiI2ODRiYzM5YjFmNmQxOGU2NTEzZmRlZGQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.9KOzF92SpnaBDZiJGNFUiJlX52JyFSTJNv1FHiJF-Yw",
+      Accept: "application/json",
+    },
+  });
+  const data = await result.json();
+  const results = data.results;
+  results.forEach((card) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `<img id="card-image" src=${baseurl_image}${card.poster_path} alt="${card.title}">`;
+    document.querySelector("#genre-three").appendChild(div);
+  });
 }
-async function seemoremovie(){
-const url=baseurl+movie  ;
-const result =await fetch(url,{
-  method:"GET", 
-  headers:{
-    Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YzBkZjBlZDNiMjIyMDgwMDc3NzM2Mzg4ODEwMGM4ZiIsIm5iZiI6MTc0OTc5NTczOS42NjIwMDAyLCJzdWIiOiI2ODRiYzM5YjFmNmQxOGU2NTEzZmRlZGQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.9KOzF92SpnaBDZiJGNFUiJlX52JyFSTJNv1FHiJF-Yw",
-    Accept:"application/json"
-  }
-})
-const data=await result.json()
-const results=data.results;
-results.forEach(card => {
-  const div=document.createElement("div");
-  div.classList.add("card")
-  div.innerHTML=`<img id="card-image" src=${baseurl_image}${card.poster_path} alt="${card.title}">`
-  document.querySelector("#see-more-section").appendChild(div)
-});
-}
-
-async function series(){
-const url=baseurl+  serie;
-const result =await fetch(url,{
-  method:"GET", 
-  headers:{
-    Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YzBkZjBlZDNiMjIyMDgwMDc3NzM2Mzg4ODEwMGM4ZiIsIm5iZiI6MTc0OTc5NTczOS42NjIwMDAyLCJzdWIiOiI2ODRiYzM5YjFmNmQxOGU2NTEzZmRlZGQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.9KOzF92SpnaBDZiJGNFUiJlX52JyFSTJNv1FHiJF-Yw",
-    Accept:"application/json"
-  }
-})
-const data=await result.json()
-const results=data.results;
-results.forEach(card => {
-  const div=document.createElement("div");
-  div.classList.add("card")
-  div.innerHTML=`<img id="card-image" src=${baseurl_image}${card.poster_path} alt="${card.title}">`
-  document.querySelector("#genre-five").appendChild(div)
-});
-}
-async function seemoreseries(){
-const url=baseurl+serie;   ;
-const result =await fetch(url,{
-  method:"GET", 
-  headers:{
-    Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YzBkZjBlZDNiMjIyMDgwMDc3NzM2Mzg4ODEwMGM4ZiIsIm5iZiI6MTc0OTc5NTczOS42NjIwMDAyLCJzdWIiOiI2ODRiYzM5YjFmNmQxOGU2NTEzZmRlZGQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.9KOzF92SpnaBDZiJGNFUiJlX52JyFSTJNv1FHiJF-Yw",
-    Accept:"application/json"
-  }
-})
-const data=await result.json()
-const results=data.results;
-results.forEach(card => {
-  const div=document.createElement("div");
-  div.classList.add("card")
-  div.innerHTML=`<img id="card-image" src=${baseurl_image}${card.poster_path} alt="${card.title}">`
-  document.querySelector("#see-more-section").appendChild(div)
-});
+async function seemoremovie() {
+  const url = baseurl + movie;
+  const result = await fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YzBkZjBlZDNiMjIyMDgwMDc3NzM2Mzg4ODEwMGM4ZiIsIm5iZiI6MTc0OTc5NTczOS42NjIwMDAyLCJzdWIiOiI2ODRiYzM5YjFmNmQxOGU2NTEzZmRlZGQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.9KOzF92SpnaBDZiJGNFUiJlX52JyFSTJNv1FHiJF-Yw",
+      Accept: "application/json",
+    },
+  });
+  const data = await result.json();
+  const results = data.results;
+  results.forEach((card) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `<img id="card-image" src=${baseurl_image}${card.poster_path} alt="${card.title}">`;
+    document.querySelector("#see-more-section").appendChild(div);
+  });
 }
 
-async function moviecollection(){
-const url=baseurl+  movie;
-const result =await fetch(url,{
-  method:"GET", 
-  headers:{
-    Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YzBkZjBlZDNiMjIyMDgwMDc3NzM2Mzg4ODEwMGM4ZiIsIm5iZiI6MTc0OTc5NTczOS42NjIwMDAyLCJzdWIiOiI2ODRiYzM5YjFmNmQxOGU2NTEzZmRlZGQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.9KOzF92SpnaBDZiJGNFUiJlX52JyFSTJNv1FHiJF-Yw",
-    Accept:"application/json"
-  }
-})
-const data=await result.json()
-const results=data.results;
-results.forEach(card => {
-  const div=document.createElement("div");
-  div.classList.add("from")
-  div.innerHTML=`<img id="card-image" src=${baseurl_image}${card.poster_path} alt="${card.title}">`
-  document.querySelector("#collection-genre").appendChild(div)
-});
+async function series() {
+  const url = baseurl + serie;
+  const result = await fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YzBkZjBlZDNiMjIyMDgwMDc3NzM2Mzg4ODEwMGM4ZiIsIm5iZiI6MTc0OTc5NTczOS42NjIwMDAyLCJzdWIiOiI2ODRiYzM5YjFmNmQxOGU2NTEzZmRlZGQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.9KOzF92SpnaBDZiJGNFUiJlX52JyFSTJNv1FHiJF-Yw",
+      Accept: "application/json",
+    },
+  });
+  const data = await result.json();
+  const results = data.results;
+  results.forEach((card) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `<img id="card-image" src=${baseurl_image}${card.poster_path} alt="${card.title}">`;
+    document.querySelector("#genre-five").appendChild(div);
+  });
 }
-async function seriescollection(){
-const url=baseurl+  serie;
-const result =await fetch(url,{
-  method:"GET", 
-  headers:{
-    Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YzBkZjBlZDNiMjIyMDgwMDc3NzM2Mzg4ODEwMGM4ZiIsIm5iZiI6MTc0OTc5NTczOS42NjIwMDAyLCJzdWIiOiI2ODRiYzM5YjFmNmQxOGU2NTEzZmRlZGQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.9KOzF92SpnaBDZiJGNFUiJlX52JyFSTJNv1FHiJF-Yw",
-    Accept:"application/json"
-  }
-})
-const data=await result.json()
-const results=data.results;
-results.forEach(card => {
-  const div=document.createElement("div");
-  div.classList.add("cast")
-  div.innerHTML=`<img id="card-image" src=${baseurl_image}${card.poster_path} alt="${card.title}">`
-  document.querySelector("#collection-genres").appendChild(div)
-});
-}
-function buttons(){
-const buttonone =document.querySelector(".button-one");
-const buttontwo =document.querySelector(".button-two");
-buttonone.classList.add("button-click")
-moviecollection()
-
-buttonone.addEventListener("click",()=>{
-buttonone.classList.add("button-click");
-buttontwo.classList.remove("button-click");
-const dom=document.querySelector("#collection-genres")
-dom.innerHTML=""
-
-moviecollection()
-
-})
-buttontwo.addEventListener("click",()=>{
-  buttontwo.classList.add("button-click");
-  buttonone.classList.remove("button-click");
-  
-    
-const dom=document.querySelector("#collection-genre")
-dom.innerHTML=""
-seriescollection()
-  
-})
-
+async function seemoreseries() {
+  const url = baseurl + serie;
+  const result = await fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YzBkZjBlZDNiMjIyMDgwMDc3NzM2Mzg4ODEwMGM4ZiIsIm5iZiI6MTc0OTc5NTczOS42NjIwMDAyLCJzdWIiOiI2ODRiYzM5YjFmNmQxOGU2NTEzZmRlZGQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.9KOzF92SpnaBDZiJGNFUiJlX52JyFSTJNv1FHiJF-Yw",
+      Accept: "application/json",
+    },
+  });
+  const data = await result.json();
+  const results = data.results;
+  results.forEach((card) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `<img id="card-image" src=${baseurl_image}${card.poster_path} alt="${card.title}">`;
+    document.querySelector("#see-more-section").appendChild(div);
+  });
 }
 
-console.log("hello world")
+async function moviecollection() {
+  const url = baseurl + movie;
+  const result = await fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YzBkZjBlZDNiMjIyMDgwMDc3NzM2Mzg4ODEwMGM4ZiIsIm5iZiI6MTc0OTc5NTczOS42NjIwMDAyLCJzdWIiOiI2ODRiYzM5YjFmNmQxOGU2NTEzZmRlZGQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.9KOzF92SpnaBDZiJGNFUiJlX52JyFSTJNv1FHiJF-Yw",
+      Accept: "application/json",
+    },
+  });
+  const data = await result.json();
+  const results = data.results;
+  results.forEach((card) => {
+    const div = document.createElement("div");
+    div.classList.add("from");
+    div.innerHTML = `<img id="card-image" src=${baseurl_image}${card.poster_path} alt="${card.title}">`;
+    document.querySelector("#collection-genre").appendChild(div);
+  });
+}
+async function seriescollection() {
+  const url = baseurl + serie;
+  const result = await fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YzBkZjBlZDNiMjIyMDgwMDc3NzM2Mzg4ODEwMGM4ZiIsIm5iZiI6MTc0OTc5NTczOS42NjIwMDAyLCJzdWIiOiI2ODRiYzM5YjFmNmQxOGU2NTEzZmRlZGQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.9KOzF92SpnaBDZiJGNFUiJlX52JyFSTJNv1FHiJF-Yw",
+      Accept: "application/json",
+    },
+  });
+  const data = await result.json();
+  const results = data.results;
+  results.forEach((card) => {
+    const div = document.createElement("div");
+    div.classList.add("cast");
+    div.innerHTML = `<img id="card-image" src=${baseurl_image}${card.poster_path} alt="${card.title}">`;
+    document.querySelector("#collection-genres").appendChild(div);
+  });
+}
+function buttons() {
+  const buttonone = document.querySelector(".button-one");
+  const buttontwo = document.querySelector(".button-two");
+  buttonone.classList.add("button-click");
+  moviecollection();
 
-switch(global.page){
+  buttonone.addEventListener("click", () => {
+    buttonone.classList.add("button-click");
+    buttontwo.classList.remove("button-click");
+    const dom = document.querySelector("#collection-genres");
+    dom.innerHTML = "";
+
+    moviecollection();
+  });
+  buttontwo.addEventListener("click", () => {
+    buttontwo.classList.add("button-click");
+    buttonone.classList.remove("button-click");
+
+    const dom = document.querySelector("#collection-genre");
+    dom.innerHTML = "";
+    seriescollection();
+  });
+}
+
+console.log("hello world");
+
+switch (global.page) {
+  case "/":
+    break;
   case "/index.html":
-    trends()
- backgroundImage()
- movies()
- series()
- buttons()
+    trends();
+    backgroundImage();
+    movies();
+    series();
+    buttons();
 
-
- break;
-
- case "/trend.html":
- seemore()
- break;
- case "/movies.html":
-  seemoremovie()
-  break;
-  case "/series.html":
-seemoreseries()
     break;
 
+  case "/trend.html":
+    seemore();
+    break;
+  case "/movies.html":
+    seemoremovie();
+    break;
+  case "/series.html":
+    seemoreseries();
+    break;
 }
